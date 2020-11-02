02d85ee8bb2155e0fd7a5a9c03f60e5fb735f513
manghuang <1255529524@qq.com>
2020-11-02 13:21:57 +0800

    class文件解析小工具

--- src/com/whw/jvm/classfile/demo3.java
+++ src/com/whw/jvm/classfile/demo3.java
@@ -3,16 +3,16 @@ package com.whw.jvm.classfile;
 public class demo3 {
 
     public static void main(String[] args) {
-//        new demo1();        //静态代码块->构造函数
-//        new demo1();        //构造函数
-//        System.out.println(demo1.NAME);   //直接输出
-//        System.out.println(demo1.PI);     //直接输出
-//        System.out.println(demo1.LIST);   //执行demo1的静态代码块
-//        new demo2();        //父类的静态代码块->自己的静态代码块->父类的构造函数->自己的构造函数
-//        new demo2();        //自己的静态代码块->自己的构造函数
-//        System.out.println(demo2.NAME);     //直接输出
-//        System.out.println(demo2.PI);       //直接输出
-//        System.out.println(demo2.LIST);     //执行demo1的静态代码块
-//        demo1[] demo1s = new demo1[2]; // 不会触发demo1的静态代码块和构造函数
+        new demo1();        //静态代码块->构造函数
+        new demo1();        //构造函数
+        System.out.println(demo1.NAME);   //直接输出
+        System.out.println(demo1.PI);     //直接输出
+        System.out.println(demo1.LIST);   //执行demo1的静态代码块
+        new demo2();        //父类的静态代码块->自己的静态代码块->父类的构造函数->自己的构造函数
+        new demo2();        //自己的静态代码块->自己的构造函数
+        System.out.println(demo2.NAME);     //直接输出
+        System.out.println(demo2.PI);       //直接输出
+        System.out.println(demo2.LIST);     //执行demo1的静态代码块
+        demo1[] demo1s = new demo1[2]; // 不会触发demo1的静态代码块和构造函数
     }
 }
---
+++ src/com/whw/jvm/classfile/demo4.java
@@ -0,0 +1,16 @@
+package com.whw.jvm.classfile;
+
+public class demo4 {
+
+    private int num = 1;
+    public static final double PI = 3.1415926;
+    public int a = 1;
+    public static final String str  = "JVM";
+    public int getNum(){
+        return num;
+    }
+
+    public void setNum(int num){
+        this.num = num;
+    }
+}
